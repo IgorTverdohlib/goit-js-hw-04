d13b66b9a80cd43919835494133f97e39237ab36
Igor Tverdohlib <162157679+IgorTverdohlib@users.noreply.github.com>
2024-06-09 16:08:31 +0300

    dz4

---
+++ js/task-2.js
@@ -0,0 +1,35 @@
+function calcAverageCalories(days) {
+  let fullCalories = 0;
+  for (const day of days) {
+    fullCalories += day.calories;
+  }
+  if (fullCalories) {
+    return fullCalories / days.length;
+  }
+  return 0;
+}
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 },
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 },
+  ])
+); // 2270
+
+console.log(calcAverageCalories([])); // 0
